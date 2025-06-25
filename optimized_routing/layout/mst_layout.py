9f0730f95821fce2aafb02c6aeb32199add8f42e
Pranjal Dhole <dhole.pranjal@gmail.com>
2025-06-25 13:35:32 +0200

    updated layout

--- optimized_routing/layout/mst_layout.py
+++ optimized_routing/layout/mst_layout.py
@@ -15,7 +15,8 @@ def generate_mst_layout(backend, qc):
     for mapping virtual qubits to physical qubits on the backend device.
     """
     # Sequence of physical qubit IDs mapped in the device layout
-    opt_layout = np.arange(0, qc.num_qubits, 1).tolist()        # FIXME This is a dummy mapping
+    #opt_layout = np.arange(0, qc.num_qubits, 1).tolist()        # FIXME This is a dummy mapping
+    opt_layout = [119, 110, 120, 118, 117]
 
     # The dictionary must be a bijective mapping between virtual qubits (tuple) and physical qubits (int).
     init_layout = Layout({q: phys for q, phys in zip(qc.qubits, opt_layout)})
